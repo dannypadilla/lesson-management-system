9117a3fb6f57a7bece1e7a2589763c433c13830f
Danny Padilla <dpadilla010@gmail.com>
2021-12-11 22:28:35 -0800

    ADD - list of students added to groups output

--- src/main/java/com/dcp/slmanager/model/StudentGroupDto.java
+++ src/main/java/com/dcp/slmanager/model/StudentGroupDto.java
@@ -1,6 +1,9 @@
 package com.dcp.slmanager.model;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,15 +14,24 @@ public class StudentGroupDto {
 	
 	private Integer studentGroupId;
 	private String groupName;
+	private List<String> studentList;
 	
 	public StudentGroupDto() {}
 	
 	public StudentGroupDto(StudentGroup studentGroup) {
 		logger.trace("Called StudentGroupDto Constructor");
+		studentList = new ArrayList<String>();
 		
 		this.studentGroupId = studentGroup.getStudentGroupId();
 		this.groupName = studentGroup.getName();
 		
+		if (studentGroup.getStudentList() != null) {
+			for (Student s : studentGroup.getStudentList() ) {
+				this.studentList.add(s.getName() );
+			}
+
+		}
+
 	}
 	
 	public Integer getStudentGroupId() {
@@ -38,4 +50,12 @@ public class StudentGroupDto {
 		this.groupName = groupName;
 	}
 	
+	public List<String> getStudentList() {
+		return this.studentList;
+	}
+
+	public void setStudentList(List<String> studentList) {
+		this.studentList = studentList;
+	}
+
 }
